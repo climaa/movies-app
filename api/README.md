410dedf0ab8928a82621380cbd026dd904dda3d1
Carlos Lima <dj.flypo@gmail.com>
2024-10-11 11:32:29 +0200

    feat(axa-api-01): added README added minor fixes on index.mjs

---
+++ api/README.md
@@ -0,0 +1,22 @@
+# API
+
+## Pre requisites - Installation
+
+You must use the `npm` as a package library; because I want to add in a standard way into github actions.
+Please Node version v20
+
+## Installation
+
+For installation; assuming your inside this folder `api`
+
+```shell
+npm install
+```
+
+## Execution or development mode
+
+Go to terminal and type
+
+```shell
+npm start
+```
--- api/src/index.mjs
+++ api/src/index.mjs
@@ -1,44 +1,48 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser'
-import {getAllMoviesFromStudios} from '../src/helpers.mjs'
-import {sony, warner, disney, movieAge} from '../constants/studio_constants.mjs'
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { getAllMoviesFromStudios } from "../src/helpers.mjs";
+import {
+  disney,
+  movieAge,
+  PORT,
+  sony,
+  warner,
+} from "../constants/studio_constants.mjs";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get('/studios', function (req, res) {
-  let disneyTemp = {...disney}
-  delete disneyTemp.movies
-  let warnerTemp = {...warner}
-  delete warnerTemp.movies
-  let sonyTemp = {...sony}
-  delete sonyTemp.movies
-  res.json([
-    disneyTemp,
-    warnerTemp,
-    sonyTemp
-  ])
+app.get("/", function (req, res) {
+  res.json({ health: "ok" });
+})
+app.get("/studios", function (req, res) {
+  let disneyTemp = { ...disney };
+  delete disneyTemp.movies;
+  let warnerTemp = { ...warner };
+  delete warnerTemp.movies;
+  let sonyTemp = { ...sony };
+  delete sonyTemp.movies;
+  res.json([disneyTemp, warnerTemp, sonyTemp]);
 });
 
-app.get('/movies', function (req, res) {
+app.get("/movies", function (req, res) {
   try {
-    res.json(getAllMoviesFromStudios([disney, warner, sony]))
+    res.json(getAllMoviesFromStudios([disney, warner, sony]));
   } catch (e) {
-    res.statusCode(500)
+    res.statusCode(500);
   }
 });
 
-app.get('/movieAge', function (req, res) {
-  res.json(movieAge)
+app.get("/movieAge", function (req, res) {
+  res.json(movieAge);
 });
 
-//TODO: 1 add the capability to sell the movie rights to another studio
-app.post('/transfer', function (req, res) {
-});
+// TODO: 1 add the capability to sell the movie rights to another studio
+app.post("/transfer", function (req, res) {});
 
 // TODO: 2 Add logging capabilities into the movies-app
-
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
